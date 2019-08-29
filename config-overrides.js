d870d4968c93edb807704ec2e9a9e08a19b85434
charliecha1990 <charliecha1990@gmail.com>
2019-08-29 11:13:31 -0400

    no

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,18 @@
 const {injectBabelPlugin} = require('react-app-rewired')
 
-module.exports = function override(config) {
+module.exports = function override(config, env) {
 
-    // injectBabelPlugin(
-    //     ['import', {
-    //         libraryName: 'antd',
-    //         libraryDirectory: 'es',
-    //         style: 'css'
-    //     }], config,
-    // )
+    injectBabelPlugin(
+        ['import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: 'css'
+        }], config,
+    )
 
     // add decorator
     config = injectBabelPlugin(
-        ['@babel/plugin-proposal-decorators', {
+        ['@Babel/plugin-proposal-decorators', {
             'legacy': true
         }],
         config,
