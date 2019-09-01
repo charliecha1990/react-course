00240a308e8292a6a60be88137d25aacecf9da46
charliecha1990 <charliecha1990@gmail.com>
2019-08-31 20:13:37 -0400

    no

--- src/components/Decorator.js
+++ src/components/Decorator.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
-export default class Decorator extends Component {
+const Context = React.createContext();
+
+const store = {
+    date: '2019',
+    logError () {
+        console.log('Errors')
+    }
+}
+
+const withConsumer = Comp => props => (
+    <Context.Provider value={store}>
+        <Context.Consumer>
+          {value => <Comp value={value} {...props}/>}   
+        </Context.Consumer>
+    </Context.Provider>
+)
+
+
+// @Consumer
+class Inner extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                {this.props.value.date}
+            </div>
+        );
+    }
+} 
+
+// @withConsumer
+class Decorator extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+               <Inner {...this.props}/>
+            </div>
         );
     }
 }
+
+export default Decorator;
