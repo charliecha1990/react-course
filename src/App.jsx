cef04e76ce538bd897dc503c07a27c25820d257d
Zha <yzha@deloitte.ca>
2019-10-07 09:43:03 -0400

    fix body-parser error

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ import DemoContext from './components/DemoContext'
 import Fundmental from './Javascript/Fundmental'
 import Hooks from './components/Hooks'
 import ImmediatelyInvokedFunctionExpression from './Javascript/ImmediatelyInvokedFunctionExpression'
-
+import SigninExample2 from './components/SigninExample2'
+import axios from 'axios';
 
 export default class App extends Component {
 
@@ -28,8 +29,11 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
+      userName: '',
+      password: '',
+      response:'',
+      seasons:['spring','summer','fall','winter'],
+      numbers:[1,2,3,4,5,6]
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -54,24 +58,32 @@ export default class App extends Component {
   //   alert(greetings)
   // }
 
-  componentDidMount() {
-
-  }
-
   handleSubmit() {
     // event.preventDefault();
-    const email = this.state.email;
-    const password = this.state.password;
+    let userName = this.state.userName;
+    let password = this.state.password;
 
-    this.authenticate(email,password)
+    this.authenticate(userName,password);
     // console.log('Credentials have been submitted')
     // console.log(email,password)
   }
 
-  authenticate(email,password){
-    setTimeout(() => {
-      alert(`Your login has been successful with email:${email}}, password:${password}`)
-    }, 3000);
+  authenticate(userName,password){
+    axios.post('/api/user',{
+      userName,
+      password
+    })
+    .then(function (response) {
+      console.log('request',response)
+      if(response.data == 'success') {
+          setTimeout(() => {
+            alert(`Your login has been successful with email:${userName}, password:${password}`)
+        }, 3000);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
  
   // handleSubmit = () => {  // Arrow functions bind this
@@ -92,6 +104,27 @@ export default class App extends Component {
     // console.log(event)
   }
 
+  createNewArray() {
+    let newArray = [];
+
+    this.state.numbers.forEach(e =>{
+      newArray.push(e+1)
+    })
+
+    this.setState({numbers: newArray})
+  }
+
+  componentDidMount() {
+    axios.get('/api/user')
+    .then(function (response) {
+      this.setState({response: response},()=> {
+        console.log(this.state.response)
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render() {
 
   const welcome = 'welcome to react'
@@ -102,7 +135,25 @@ export default class App extends Component {
 
     return (
       <div>
-        <ImmediatelyInvokedFunctionExpression />
+        {/* <ImmediatelyInvokedFunctionExpression /> */}
+        <SigninExample2 
+          onSubmit={this.handleSubmit}
+          userName={this.state.userName}
+          password={this.state.password}
+          onChange={this.handleChange}
+          response={this.state.response}
+        />
+        {/* {this.state.seasons.map(season => <div key={season}>{season+'2019'}</div>)}
+
+        {console.log(this.state.seasons)}
+        {this.state.seasons.filter(season => { return season!=='spring' } )} */}
+        {/* {this.state.numbers.forEach(number => this.state.numbers.push(number)) } */}
+        {/* {console.log(this.state.numbers)} */}
+        {/* ForEach针对逻辑层*/}
+    
+
+        {/* 内部成员属性值不改变*/}
+
         {/* <Hooks /> */}
         {/* <Fundmental /> */}
         {/* <DemoContext /> */}
